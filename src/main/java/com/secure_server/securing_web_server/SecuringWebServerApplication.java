a1b0678bc796002a3eecb233dbe051fc2d6afb70
MRINMOY_1315 <mrinmoymatilal1315@gmail.com>
2025-04-23 01:57:22 +0530

    Add files via upload

---
+++ src/main/java/com/secure_server/securing_web_server/SecuringWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.secure_server.securing_web_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringWebServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secure_server/securing_web_server/SecuringWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure_server.securing_web_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
